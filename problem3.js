bc9b91ea8b0c24ccf6331a51937713aa60a511cd
Martin Fuska <martin.fuska@gmail.com>
2014-08-22 17:54:16 +0200

    problem 3 optimizations

--- problem3.js
+++ problem3.js
@@ -1,44 +1,49 @@
 function isPrime(n) {
 
-   if (n < 2) {
-      return false
-   }
-   
-   var isPrime = true,
-      i = 2,
-      l = Math.sqrt(n);
+    if (n < 2) {
+       return false
+    }
 
-   for (i; i <= l; i++) {
-      if (n % i == 0) {isPrime = false}
-   }
+    var i = 3,
+       l = Math.sqrt(n);
 
-   return isPrime;
+    if (n % 2 === 0) {
+        return true;
+    } else {
+        for (i; i <= l; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+    }
+
+    return true;
 
 }
 
 function nextPrime(n) {
 
-   n++;
+    n++;
 
-   while (!isPrime(n)) {
-      n++;
-   }
+    while (!isPrime(n)) {
+        n++;
+    }
 
-   return n;
+    return n;
 
 }
 
 var number = 600851475143,
-   divider = 2,
-   t1 = process.hrtime()[1];
+divider = 2,
+t1 = process.hrtime()[1];
 
 while (!isPrime(number)) {
-   
-   if ((number % divider === 0)) {
-      number = number / divider;
-   } else {
-      divider = nextPrime(divider);
-   }
+
+    if ((number % divider === 0)) {
+        number = number / divider;
+    } else {
+        divider = nextPrime(divider);
+    }
 
 }
 
